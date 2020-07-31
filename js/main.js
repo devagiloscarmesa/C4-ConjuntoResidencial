d042ac2c9522e64dddeca4a0256b46249487e690
oscar_mesa <oscarmesa.elpoli@gmail.com>
2020-07-31 13:09:43 -0500

    cambios

--- js/main.js
+++ js/main.js
@@ -1,3 +1,31 @@
+const DATOS_FORMULARIO = [
+    {
+        datos_propietario : {
+            nombre : "Juan", 
+            documento : "Perez", 
+            genero : "M", 
+            fech_nac : "10-10-1980"
+        }, 
+        datos_inmueble : {
+            metos_cuadrados : 30, 
+            tipo : "C", 
+            numero_habitantes : 10, 
+            fech_mensualidad : "10-0-2020"
+        }
+    }
+];
+/*
+numeros.map(function(elemento, p){  
+	console.log("=====");
+    console.log(p, elemento);
+	console.log("=====");
+    return "posición: "+elemento;
+})
+
+[1,2,3,4,5]
+["posición: 1","posición: 2","posición: 3","posición: 4","posición: 5"]
+*/
+	
 document.getElementById("btn-guardar-inmueble").addEventListener('click', (e) =>{
 	let nombre_almacenado, documento_almacenado, 
             genero_almacenado, fecha_nac_almacenada,mt_inmueble_almacenado,tipo_inmueble_almacenado, 
@@ -32,14 +60,14 @@ document.getElementById("btn-guardar-inmueble").addEventListener('click', (e) =>
         }else{
             inmueble.datos_inmueble.tipo = document.getElementById("tipo_casa").value;
         }
-
-        
-
     }else{
         alert("Error de validación de campos.")
     }
-    
-    console.log(inmueble);
+    DATOS_FORMULARIO.push(inmueble);
+    document.getElementById("frm-mensualidad").reset();
+    alert("Los datos fueron almacenados");
+    $('#modal-adicionar').modal('toggle');
+        
 });
    	
 $(document).ready((e) => {
