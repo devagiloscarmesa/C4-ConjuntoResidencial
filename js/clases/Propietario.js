15884ea352b98c961db60f355a591f5bb0cd8b3a
oscar_mesa <oscarmesa.elpoli@gmail.com>
2020-08-06 08:40:09 -0500

    clase inmueble y propietario

---
+++ js/clases/Propietario.js
@@ -0,0 +1,11 @@
+import Inmueble from './Inmueble.js';
+export default class Propietario{
+    constructor(nombre, documento, genero, fecha_nacimiento){
+        this.nombre = nombre;
+        this.documento = documento;
+        this.genero = genero;
+        this.fecha_nacimiento = fecha_nacimiento;
+    }
+    
+    //asignarImueble(...)
+}
